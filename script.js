6a459c8ed0142f354fed1df57bec5cc67d9c5f32
reszka303 <reszka303@op.pl>
2020-01-03 17:09:16 +0100

    Final deployment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://stark-mountain-98426.herokuapp.com/v1/task/';
+  const apiRoot = 'https://stark-mountain-98426.herokuapp.com/v1/tasks/';
   const trelloApiRoot = 'https://stark-mountain-98426.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
